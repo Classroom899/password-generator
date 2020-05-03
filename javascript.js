d228143caeb6ae3b125623c78c8290f4d6ecf0a7
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-05-03 15:46:02 -0700

    Add criteria needed into Javascript

--- javascript.js
+++ javascript.js
@@ -9,6 +9,28 @@ var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%
 
 var password = "";
 
+// Add prompts of what questions we need for our password
+
+
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -20,3 +42,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
